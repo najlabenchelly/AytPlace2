0f14f4d96c06631f2140be17a15c6914b514486a
najlabenchelly <chelly.n103@gmail.com>
2019-06-09 00:24:33 +0200

    evolution annonce avec imagee

--- AytPlace2/src/DataFixtures/AppFixtures.php
+++ AytPlace2/src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -29,15 +30,26 @@ class AppFixtures extends Fixture
            ->setContent($content)
            ->setPrice(mt_rand(40,200))
            ->setRooms(mt_rand(1, 5));
-        
 
+           //creation image relieer à ad 
+   
+        for($j= 1; $j <= mt_rand(2, 5); $j++) {
+            $image = new Image();
+            $image->setUrl($faker->imageUrl())
+                  ->setCaption($faker->sentence())
+                  ->setAd($ad);
+
+            $manager->persist($image);
+        }
+        
+      
         $manager->persist($ad);
     }
 
         $manager->flush();
 
     
-        
+    
 
         // $manager->persist($product);
 
