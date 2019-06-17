3103db358f80cdb1699be73530b3a4a760e06212
najlabenchelly <chelly.n103@gmail.com>
2019-06-17 22:24:20 +0200

    page de reservation enregistrement bdd / et vu

---
+++ AytPlace2/src/Controller/BookingController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Entity\Booking;
+use App\Entity\Comment;
+use App\Form\BookingType;
+use App\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BookingController extends AbstractController
+{
+    /**
+     * @Route("/ads/{slug}/book", name="booking_create")
+     * @IsGranted("ROLE_USER")
+     */
+    public function book(Ad $ad, Request $request,ObjectManager $manager)
+
+    {
+        $booking = new Booking();
+        $form =$this->createForm(BookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $this->getUser();
+
+            $booking->setBooker($user)
+                    ->setAd($ad);
+
+            $manager->persist($booking);
+            $manager->flush();
+
+            return $this->redirectToRoute('booking_show',['id'=> $booking->getId()]);
+
+        }
+
+        return $this->render('booking/book.html.twig', [
+            'ad' => $ad,
+            'form'=> $form->createView()
+        ]);
+    }
+    /**
+     * affichage de la page réservation 
+     *@Route("/booking/{id}",name="booking_show")
+     * @param Booking $Booking
+     * @return Response
+     */
+    public function show(Booking $Booking){
+        return $this->render("booking/show.html.twig",[
+
+            'booking'=>$Booking
+        ]);
+
+    }
+}
