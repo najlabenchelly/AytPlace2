4afa69b1781899eabe04e016ff9ec22c4ab30292
najlabenchelly <chelly.n103@gmail.com>
2019-06-22 19:38:12 +0200

    Javascript et reestriction date sur form reservation

--- AytPlace2/src/Entity/User.php
+++ AytPlace2/src/Entity/User.php
@@ -75,7 +75,7 @@ class User implements UserInterface
     private $description;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable = true)
+     * @ORM\Column(type="string", length=255)
      */
     private $slug;
 
