59806de2c2711bde5b3b66d304c9fcdf36142d83
najlabenchelly <chelly.n103@gmail.com>
2019-07-20 17:31:14 +0200

    tableau de bord ajout recup nombree utilisateur annonces avis + tableau   meilleurs annoncees

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,18 +2,51 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminDashboardController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin_dashboard")
      */
-    public function index()
+    public function index(ObjectManager $manager )
     {
+
+      // requête Dql 
+        $users =$manager->createQuery('SELECT COUNT(u) FROM App\Entity\User u')->getSingleScalarResult();
+        $ads =$manager->createQuery('SELECT COUNT(a) FROM App\Entity\Ad a')->getSingleScalarResult();
+        $bookings =$manager->createQuery('SELECT COUNT(b) FROM App\Entity\Booking b')->getSingleScalarResult();
+        $comments =$manager->createQuery('SELECT COUNT(c) FROM App\Entity\Comment c')->getSingleScalarResult();
+        
+        $bestAds = $manager->createQuery(
+            
+            'SELECT AVG(c.rating)as note, a.title, a.id, u.firstname, u.lastname, u.picture 
+            FROM App\Entity\Comment c
+            JOIN c.ad a
+            JOIN a.author u
+            GROUP BY a 
+            ORDER BY note DESC'
+            )
+            ->setMaxResults(5)
+            ->getResult();
+
+            dump($bestAds);
+
+
+
+ 
+
         return $this->render('admin/dashboard/index.html.twig', [
-            'controller_name' => 'AdminDashboardController',
+            'stats' =>[
+            'users' => $users,
+            'ads'=>$ads,
+            'bookings'=>$bookings,
+            'comments'=> $comments
+            
+            ],
+            'bestAds'=> $bestAds
         ]);
     }
 }
